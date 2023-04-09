c6a9924fb137084d71b8ed8af0ce55c947e9b6bf
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2023-04-09 18:57:23 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+
+let A = Int(lineArr[0])!
+let B = Int(lineArr[1])!
+
+if A > B {
+    print(">")
+} else if A < B {
+    print("<")
+} else if A == B {
+    print("==")
+}
