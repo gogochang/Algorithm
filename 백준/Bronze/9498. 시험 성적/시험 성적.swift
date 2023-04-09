a1a7e697991f0066c5ca0959ea33ea9fc53bea36
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2023-04-09 19:10:42 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,13 @@
+let input = Int(readLine()!)!
+
+if 90 <= input  {
+    print("A")
+} else if 80 <= input {
+    print("B")
+} else if 70 <= input {
+    print("C")
+} else if 60 <= input {
+    print("D")
+} else {
+    print("F")
+}
