4acbffc86cddae14e3c1e1c51748b23fa5db4093
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2023-04-09 19:20:03 +0900

    [Bronze V] Title: 윤년, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.swift
+++ 백준/Bronze/2753. 윤년/윤년.swift
@@ -1,5 +1,5 @@
 let input = Int(readLine()!)!
-if ((input % 4 == 0) && (input % 400 == 0)) || ((input % 4 == 0) && (input % 100 != 0)) {
+if (input % 4 == 0) && ((input % 400 == 0) || (input % 100 != 0)) {
     print(1)
 } else {
     print(0)
