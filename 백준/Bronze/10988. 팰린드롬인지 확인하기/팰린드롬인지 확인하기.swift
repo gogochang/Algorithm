0625889bf0900ea6e175d594be0710a3a97350a3
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2023-04-12 23:03:00 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.swift
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.swift
@@ -1,10 +1,7 @@
 while let input = readLine() {
-    let word = input.map{$0}
-    var result = 1
-    for i in 0..<word.count/2 {
-        if word[word.startIndex + i] != word[word.index(before: word.endIndex) - i] {
-            result = 0
-        }
+    if String(input.reversed()) == input {
+        print(1)
+    } else {
+        print(0)
     }
-    print(result)
 }
