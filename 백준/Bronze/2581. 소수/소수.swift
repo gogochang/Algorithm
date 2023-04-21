d1ce55df43c9c8d0903d5dcf226bf74c4d943d4e
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2023-04-21 16:54:24 +0900

    [Bronze II] Title: 소수, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.swift
+++ 백준/Bronze/2581. 소수/소수.swift
@@ -1,18 +1,24 @@
+import Foundation
+
 let N = Int(readLine()!)!
 let M = Int(readLine()!)!
 var result: [Int] = []
 
 for i in N...M {
+    if i == 1 {
+        continue
+    }
     var count = 0
-    for j in 1...i {
+    for j in 1...Int(sqrt(Double(i))) {
         if i % j == 0 {
             count += 1
         }
     }
-    if count == 2 {
+    if count == 1 {
         result.append(i)
     }
 }
+
 if result.isEmpty {
     print(-1)
 } else {
