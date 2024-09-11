bfb9c466897a9d6a23b3ad67a6c8bb37b2b28f57
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-11 21:54:05 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 8 ms, Memory: 69280 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.swift
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.swift
@@ -1,13 +1,11 @@
-let input = readLine()!.split(separator:" ").map { Int($0)! }
-let N = input[0]
-let K = input[1]
-var arr: [Int] = Array(1...N)
-var results: [Int] = []
-var index: Int = 0
+var input = readLine()!.split(separator: " ").map { Int($0)!}
+var index = 0
+var arr = Array(1...input[0])
+var answer: [Int] = []
 
-(0..<N).forEach { _ in
-  index = (index+(K-1)) % arr.count
-  results.append(arr.remove(at: index))
+while !arr.isEmpty {
+    index = (index+input[1]-1)%arr.count
+    answer.append(arr.remove(at: index))
 }
 
-print("<\(results.map{String($0)}.joined(separator: ", "))>")
+print("<\(answer.map {"\($0)"}.joined(separator: ", "))>")
