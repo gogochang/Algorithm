68c255b54a7c4fb0cb5c3f92d53af4c988eeced3
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-12 12:35:42 +0900

    [Silver II] Title: 쇠막대기, Time: 16 ms, Memory: 81168 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.swift
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.swift
@@ -1,19 +1,33 @@
-let input = readLine()!.map {$0}
-var stack: [Character] = []
-var sum: Int = 0
+import Foundation
 
-for i in 0..<input.count {
-  if input[i] == "(" {
-    stack.append(input[i])
-  } else if input[i] == ")" {
-    if input[i-1] == "(" {
-      stack.removeLast()
-      sum += stack.count
-    } else {
-      stack.removeLast()
-      sum += 1
+// 문자열 입력 받기
+let input = readLine()!
+
+// 스택과 잘린 조각 수 초기화
+var stack: [Character] = []
+var preChar: Character?
+var pieces = 0
+
+// 문자열을 순회
+for char in input {
+    if char == "(" {
+        // 여는 괄호는 스택에 추가
+        stack.append(char)
+    } else if char == ")" {
+        // 닫는 괄호 처리
+        if preChar == "(" {
+            // 레이저를 만났을 때
+            stack.removeLast() // 여는 괄호 제거
+            pieces += stack.count // 현재 스택에 있는 조각 수 추가
+        } else {
+            // 조각의 끝을 만났을 때
+            stack.removeLast() // 여는 괄호 제거
+            pieces += 1 // 새로운 조각 추가
+        }
     }
-  }
-  
+    
+    preChar = char
 }
-print(sum)
+
+// 결과 출력
+print(pieces)
