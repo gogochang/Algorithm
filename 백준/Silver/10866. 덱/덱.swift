d38e86e69706d3056aedcd5ec66df888e859d47f
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-12 12:37:12 +0900

    [Silver IV] Title: 덱, Time: 24 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.swift
+++ 백준/Silver/10866. 덱/덱.swift
@@ -1,35 +1,66 @@
-let N = Int(readLine()!)!
-var deque: [Int] = []
-var outputs: [Int] = []
-(0..<N).forEach { _ in
-  let action = readLine()!
-  
-  switch action {
-  case "pop_front":
-    outputs.append(deque.isEmpty ? -1 : deque.removeFirst())
-  case "pop_back":
-    outputs.append(deque.isEmpty ? -1 : deque.removeLast())
-  case "size":
-    outputs.append(deque.count)
-  case "empty":
-    outputs.append(deque.isEmpty ? 1 : 0)
-  case "front":
-    outputs.append(deque.isEmpty ? -1 : deque[0])
-  case "back":
-    outputs.append(deque.isEmpty ? -1 : deque.last!)
-  default:
-    let action = action.split(separator:" ").map { $0 }
-    var number: [Int] = [Int(action[1])!]
-    
-    if action[0] == "push_front" {
-      deque = number + deque
-    } else if action[0] == "push_back" {
-      deque = deque + number
-    }
-    return
-  }
+enum Command {
+    case push_front, push_back, pop_front, pop_back, size, empty, front, back
 }
 
-outputs.forEach {
-  print($0)
+struct Deque {
+    private var deque: [Int] = []
+    
+    mutating func push_front(_ element: Int) {
+        deque.insert(element, at: 0)
+    }
+    
+    mutating func push_back(_ element: Int) {
+        deque.append(element)
+    }
+    
+    mutating func pop_front() -> Int {
+        return deque.isEmpty ? -1 : deque.removeFirst()
+    }
+    
+    mutating func pop_back() -> Int {
+        return deque.isEmpty ? -1 : deque.removeLast()
+    }
+    
+    var size: Int {
+        deque.count
+    }
+    
+    var empty: Int {
+        deque.isEmpty ? 1 : 0
+    }
+    
+    var front: Int {
+        deque.first ?? -1
+    }
+    
+    var back: Int {
+        deque.last ?? -1
+    }
+}
+
+let n = Int(readLine()!)!
+var deque = Deque()
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ")
+    switch input[0] {
+    case "push_front":
+        deque.push_front(Int(input[1])!)
+    case "push_back":
+        deque.push_back(Int(input[1])!)
+    case "pop_front":
+        print(deque.pop_front())
+    case "pop_back":
+        print(deque.pop_back())
+    case "size":
+        print(deque.size)
+    case "empty":
+        print(deque.empty)
+    case "front":
+        print(deque.front)
+    case "back":
+        print(deque.back)
+    default:
+        break
+    }
 }
