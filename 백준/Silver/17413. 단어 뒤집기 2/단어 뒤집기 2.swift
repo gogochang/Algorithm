87551bc7e6e939eadbbe642bfe6078765c24270f
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-12 11:06:48 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 32 ms, Memory: 71256 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.swift
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.swift
@@ -1,30 +1,37 @@
-let S = readLine()!
-var result: String = ""
-var temp: String = ""
-var isStart: Bool = false
+var input = readLine()!.map { String($0) }
+var tag = ""
+var word = ""
+var isTag: Bool = false
+var answer = ""
 
-S.forEach {
-  if $0 == "<" {
-    if !temp.isEmpty {
-      result.append(String(temp.reversed()))
-      temp = ""
+for char in input {
+    switch char {
+    case "<":
+        if word != "" {
+            answer.append(String(word.reversed()))
+            word = ""
+        }
+        tag.append(char)
+        isTag = true
+    case ">":
+        tag.append(char)
+        answer.append(tag)
+        tag = ""
+        isTag = false
+    case " ":
+        if isTag {
+            tag.append(char)
+        } else {
+            answer.append(String(word.reversed() + " "))
+            word = ""
+        }
+    default:
+        if isTag {
+            tag.append(char)
+        } else {
+            word.append(char)
+        }
     }
-    isStart = true
-  }
-  
-  if isStart {
-    result.append($0)
-    if $0 == ">" {
-      isStart = false
-    }
-  } else {
-    if $0 == " " {
-      result.append(String(temp.reversed()) + " ")
-      temp = ""
-    } else {
-      temp.append($0)
-    }
-  }
 }
 
-print(result + String(temp.reversed()))
+print(answer + word.reversed())
