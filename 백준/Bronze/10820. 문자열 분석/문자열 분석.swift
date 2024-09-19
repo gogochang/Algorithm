4ae8014963603262ca1077cfe7893bdc511787fd
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-19 19:21:28 +0900

    [Bronze II] Title: 문자열 분석, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.swift
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.swift
@@ -1,16 +1,20 @@
 while let S = readLine() {
-  var result: [Int] = Array(repeating: 0, count: 4)
-  for char in S {
-    let ascii = Int(char.asciiValue!)
-    if 65 <= ascii && 90 >= ascii {
-      result[1] += 1
-    } else if 97 <= ascii && 122 >= ascii {
-      result[0] += 1
-    } else if 48 <= ascii && 57 >= ascii {
-      result[2] += 1
-    } else {
-      result[3] += 1
+    var lower = 0
+    var upper = 0
+    var num = 0
+    var space = 0
+    
+    for i in S {
+        if i >= "a" && i <= "z" {
+            lower += 1
+        } else if i >= "A" && i <= "Z" {
+            upper += 1
+        } else if i == " " {
+            space += 1
+        } else {
+            num += 1
+        }
     }
-  }
-  print(result.map { String($0)}.joined(separator: " "))
+    
+    print("\(lower) \(upper) \(num) \(space)")
 }
