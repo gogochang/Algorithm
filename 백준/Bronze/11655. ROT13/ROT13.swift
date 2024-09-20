60d02c8c5c82237581c4ed7cc0be24a134e2356b
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-20 10:14:13 +0900

    [Bronze I] Title: ROT13, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.swift
+++ 백준/Bronze/11655. ROT13/ROT13.swift
@@ -1,16 +1,15 @@
-let S = readLine()!
-var result: [Character] = []
-for char in S {
-  var ascii = Int(char.asciiValue!)
-  if (65 <= ascii && 90 >= ascii) {
-    ascii += 13
-    ascii = (ascii > 90) ? 65+(ascii-91) : ascii
-  } else if (97 <= ascii && 122 >= ascii) {
-    ascii += 13
-    ascii = (ascii > 122) ? 97+(ascii-123) : ascii
-    
-  }
-  result.append(Character(UnicodeScalar(ascii)!))
-}
+let s = readLine()!
 
-print(result.map { String($0)}.joined(separator:""))
+for char in s {
+    let ascii = char.asciiValue!
+    
+    if ascii >= 65 && ascii <= 90 {
+        let output = ((ascii + 13) - 65) % 26 + 65
+        print(Character(UnicodeScalar(output)), terminator: "")
+    } else if ascii >= 97 && ascii <= 122 {
+        let output = ((ascii + 13) - 97) % 26 + 97
+        print(Character(UnicodeScalar(output)), terminator: "")
+    } else {
+        print(char, terminator: "")
+    }
+}
