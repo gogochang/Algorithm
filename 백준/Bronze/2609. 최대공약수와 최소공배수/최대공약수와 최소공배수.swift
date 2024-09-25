a34a8cc80eb824bedacc6bfeae24ff319b051de2
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-25 15:54:06 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.swift
@@ -1,20 +1,26 @@
-let input = readLine()!.split(separator: " ").map { Int($0)! }.sorted(by: >)
-var A = input[0]
-var B = input[1]
-
-func findGCD(_ a: Int, _ b: Int) -> Int {
-    var x = a
-    var y = b
-    while y != 0 {
-        let r = x % y
-        x = y
-        y = r
+// 최대공약수 (유클리드 호제법)
+func gcd(_ a: Int, _ b: Int) -> Int {
+    var a = a
+    var b = b
+    // b가 0이 될 때 까지 반복
+    while b != 0 {
+        let remainder = a%b
+        a = b
+        b = remainder
     }
     
-    return x
+    return a
 }
-let gcd = findGCD(input[0], input[1])
-let lcm = input[0] * input[1] / gcd
 
-print(gcd)
-print(lcm)
+// 최소공배수
+func lcm(_ a: Int, _ b: Int) -> Int {
+    return a * b / gcd(a, b)
+}
+
+let inputs = readLine()!.split(separator: " ").map { Int($0)! }
+let a = inputs[0]
+let b = inputs[1]
+
+print(gcd(a, b))
+print(lcm(a, b))
+
