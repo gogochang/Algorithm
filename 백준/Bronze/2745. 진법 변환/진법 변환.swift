9be0fba1ea54d4dadc2b286b8e9aa7b6d9b29db7
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-09-30 14:13:19 +0900

    [Bronze II] Title: 진법 변환, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.swift
+++ 백준/Bronze/2745. 진법 변환/진법 변환.swift
@@ -1,2 +1,16 @@
+let digits = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
 let input = readLine()!.split(separator: " ")
-print(Int(input[0], radix: Int(input[1])!)!)
+var N = input[0]
+let B = Int(input[1])!
+var nBase = 1
+var result = 0
+
+for num in N.reversed() {
+    let currentNum = Int(digits.firstIndex(of: String(num))!)
+    result += (currentNum * nBase)
+    nBase *= B
+}
+print(result)
+
+
