f1d15fc55ed38ae41de7d1df193e21fd616dbcca
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-10-01 15:51:54 +0900

    [Bronze I] Title: 소인수분해, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.swift
+++ 백준/Bronze/11653. 소인수분해/소인수분해.swift
@@ -1,10 +1,16 @@
-var input = Int(readLine()!)!
-while input > 1 {
-    for i in 2...input {
-        if input % i == 0 {
-            input = input/i
-            print(i)
+import Foundation
+
+var N = Int(readLine()!)!
+if N > 1 {
+    for i in 2...N {
+        if N <= 1 {break}
+        if i * i > N {
+            print(N)
             break
+        }
+        while(N % i == 0) {
+            print(i)
+            N /= i
         }
     }
 }
