9ccfb9bc2c14557dd8b7188452741fcd2c9fa3d3
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-10-02 13:04:05 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 196 ms, Memory: 80488 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.swift
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.swift
@@ -1,36 +1,44 @@
-let N = 1000000
-var nums = Array(0...N)
-nums[1] = 0
+import Foundation
 
-for i in 2..<N {
-    if nums[i] == 0 {
-        continue
-    }
+// 에라토스테네스의 체를 이용해 소수를 출력합니다.
+func sieveOfEratosthenes(limit: Int) -> [Bool] {
+    var isPrime = Array(repeating: true, count: limit+1)
+    // 0과 1은 소수가 아니므로 false로 설정
+    isPrime[0] = false
+    isPrime[1] = false
     
-    for j in stride(from: i+i, through: N, by: i) {
-        nums[j] = 0
-    }
-}
-
-let primeNums = nums.map { $0 != 0 }
-
-while true {
-    let n = Int(readLine()!)!
-    if n == 0 { break }
-    var isGoldbach = false
-    
-    for i in 3...n {
-        if primeNums[i] {
-            let diff = n - i
-            if primeNums[diff] {
-                isGoldbach = true
-                print("\(n) = \(i) + \(diff)")
-                break
+    for i in 2...limit {
+        if isPrime[i] {
+            for multiple in stride(from: i*i, through: limit, by: i) {
+                isPrime[multiple] = false
             }
         }
     }
     
-    if !isGoldbach {
+    return isPrime
+}
+
+let maxLimit = 1000000
+let isPrime = sieveOfEratosthenes(limit: maxLimit)
+
+while true {
+    let n = Int(readLine()!)!
+    var found = false
+    
+    if n == 0 {
+        break
+    }
+    
+    // 3부터 시작해서 두 소수의 합으로 n을 표현할 수 있는지 확인
+    for i in 3...n {
+        if isPrime[i], isPrime[n - i] {
+            print("\(n) = \(i) + \(n - i)")
+            found = true
+            break
+        }
+    }
+    
+    if !found {
         print("Goldbach's conjecture is wrong.")
     }
 }
