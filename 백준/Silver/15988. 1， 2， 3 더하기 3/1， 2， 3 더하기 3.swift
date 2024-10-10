f596df0fda258f3fee2bee707d60efb2b7392790
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-10-10 15:00:32 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 36 ms, Memory: 76916 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.swift
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.swift
@@ -1,26 +1,17 @@
 let t = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 1000001)
 let mod = 1000000009
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in 4...1000000 {
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % mod
+}
+
 for _ in 0..<t {
     let n = Int(readLine()!)!
-    var dp = Array(repeating: 0, count: n+1)
-    
-    if n == 0 {
-        dp[n] = 0
-    } else if n == 1 {
-        dp[n] = 1
-    } else if n == 2 {
-        dp[n] = 2
-    } else if n == 3 {
-        dp[n] = 4
-    } else {
-        dp[0] = 0
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
-        
-        for i in 4...n {
-            dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % mod
-        }
-    }
     print(dp[n])
 }
