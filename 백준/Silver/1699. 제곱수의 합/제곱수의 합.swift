e04f6e25c3318c55903d71a6a5c778db600ce39e
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-10-10 11:19:43 +0900

    [Silver II] Title: 제곱수의 합, Time: 360 ms, Memory: 69884 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.swift
@@ -0,0 +1,13 @@
+let n = Int(readLine()!)!
+var dp = Array(repeating: Int.max, count: n + 1)
+dp[0] = 0
+
+for i in 1...n {
+    var j = 1
+    while j*j <= i {
+        dp[i] = min(dp[i], dp[i-(j*j)] + 1)
+        j += 1
+    }
+}
+
+print(dp[n])
