a573d24cdc816aa3ab2a54dd02f5457e7e39630b
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-10-26 10:31:02 +0900

    [Silver II] Title: N과 M (11), Time: 148 ms, Memory: 76992 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.swift
@@ -0,0 +1,30 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let N = input[0]
+let M = input[1]
+let numbers: [Int] = readLine()!.split(separator: " ").compactMap { Int($0) }.sorted()
+var stack = [String]()
+var result = ""
+var visited = Array(repeating: false, count: N)
+
+func backtrack(_ depth: Int) {
+    if M == depth {
+        result += stack.joined(separator: " ") + "\n"
+        return
+    }
+    
+    var lastUsed = -1 // 마지막으로 사용한 숫자를 저장
+    for i in 0..<N {
+        if numbers[i] == lastUsed {
+            continue
+        }
+        
+        stack.append(String(numbers[i]))
+        backtrack(depth + 1)
+        stack.removeLast()
+        lastUsed = numbers[i]
+        
+    }
+}
+
+backtrack(0)
+print(result)
