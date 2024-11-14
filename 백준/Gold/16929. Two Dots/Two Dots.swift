eb9bac3803bb522f51dcf78b7adc2a7ccc2ec17c
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-11-14 11:54:17 +0900

    [Gold IV] Title: Two Dots, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/16929. Two Dots/Two Dots.swift
+++ 백준/Gold/16929. Two Dots/Two Dots.swift
@@ -1,52 +1,48 @@
-import Foundation
-
 let nm = readLine()!.split(separator: " ").map { Int($0)! }
 let n = nm[0]
 let m = nm[1]
 
 var board = [[Character]]()
 for _ in 0..<n {
-    let row = Array(readLine()!)
-    board.append(row)
+    board.append(Array(readLine()!))
 }
-
-var visited = Array(repeating: Array(repeating: false, count: m), count: n)
-let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
+let directions = [(-1,0), (1,0), (0, -1), (0, 1)]
 
 func isInBounds(_ x: Int, _ y: Int) -> Bool {
     return x >= 0 && x < n && y >= 0 && y < m
 }
 
+// dfs
 func dfs(_ x: Int, _ y: Int, _ startX: Int, _ startY: Int, _ color: Character) -> Bool {
     if visited[x][y] {
         return true
     }
-
+    
     visited[x][y] = true
-
-    for direction in directions {
-        let newX = x + direction.0
-        let newY = y + direction.1
-
-        if isInBounds(newX, newY), board[newX][newY] == color {
-            if !(newX == startX && newY == startY) { // If it is not the previous cell
-                if dfs(newX, newY, x, y, color) {
+    
+    for (dx, dy) in directions {
+        let nx = x + dx
+        let ny = y + dy
+        
+        if isInBounds(nx, ny), board[nx][ny] == color {
+            if !(nx == startX && ny == startY) {
+                if dfs(nx, ny, x, y, color) {
                     return true
                 }
             }
         }
     }
-
+    
+    visited[x][y] = false
     return false
 }
 
 func findCycle() -> Bool {
     for i in 0..<n {
         for j in 0..<m {
-            if !visited[i][j] {
-                if dfs(i, j, -1, -1, board[i][j]) {
-                    return true
-                }
+            if dfs(i, j, -1, -1, board[i][j]) {
+                return true
             }
         }
     }
