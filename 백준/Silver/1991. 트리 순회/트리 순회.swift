65fb02d4eee700a894007fbf87aa222ad525b538
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-11-21 10:31:45 +0900

    [Silver I] Title: 트리 순회, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.swift
+++ 백준/Silver/1991. 트리 순회/트리 순회.swift
@@ -1,20 +1,19 @@
-import Foundation
 
 // Node 정의
-class Node {
+final class Node {
     let value: Character
     var left: Node?
     var right: Node?
-
+    
     init(value: Character) {
         self.value = value
     }
 }
 
 // 트리 클래스 정의
-class BinaryTree {
+final class BinaryTree {
     private var nodes: [Character: Node] = [:]
-
+    
     // 노드 생성 또는 반환
     func getNode(_ value: Character) -> Node {
         if let node = nodes[value] {
@@ -25,42 +24,43 @@ class BinaryTree {
             return newNode
         }
     }
-
+    
     // 트리에 연결 추가
     func addEdge(parent: Character, left: Character, right: Character) {
         let parentNode = getNode(parent)
         if left != "." {
             parentNode.left = getNode(left)
         }
+        
         if right != "." {
             parentNode.right = getNode(right)
         }
     }
-
-    // 전위 순회
+    
+    // 전위 순회 (루트)(왼쪽자식노드)(오른쪽자식노드)
     func preorder(_ node: Node?, _ result: inout String) {
         guard let node = node else { return }
         result.append(node.value)
         preorder(node.left, &result)
         preorder(node.right, &result)
     }
-
-    // 중위 순회
+    
+    // 중위 순회 (왼쪽자식노드)(루트)(오른쪽자식노드)
     func inorder(_ node: Node?, _ result: inout String) {
         guard let node = node else { return }
         inorder(node.left, &result)
         result.append(node.value)
         inorder(node.right, &result)
     }
-
-    // 후위 순회
+    
+    // 후위 순회 (왼쪽자식노드)(오른쪽자식노드)(루트)
     func postorder(_ node: Node?, _ result: inout String) {
         guard let node = node else { return }
         postorder(node.left, &result)
         postorder(node.right, &result)
         result.append(node.value)
     }
-
+    
     // 순회 실행 및 결과 반환
     func traverse(root: Character) -> (String, String, String) {
         let rootNode = nodes[root]
@@ -68,7 +68,7 @@ class BinaryTree {
         preorder(rootNode, &pre)
         inorder(rootNode, &ino)
         postorder(rootNode, &post)
-        return (pre, ino, post)
+        return(pre, ino, post)
     }
 }
 
@@ -77,12 +77,11 @@ let n = Int(readLine()!)!
 let tree = BinaryTree()
 
 for _ in 0..<n {
-    let input = readLine()!.split(separator: " ").map { Character(String($0)) }
+    let input = readLine()!.split(separator: " ").map {Character(String($0)) }
     tree.addEdge(parent: input[0], left: input[1], right: input[2])
 }
 
-// 결과 출력
 let root = tree.traverse(root: "A")
-print(root.0)  // 전위 순회
-print(root.1)  // 중위 순회
-print(root.2)  // 후위 순회
+print(root.0) // 전위 순회
+print(root.1) // 중위 순회
+print(root.2) // 후위 순회
