1ca512c7c3a62035a2e22aeacee58aec79b0466b
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-12-03 15:59:55 +0900

    [Silver I] Title: 절댓값 힙, Time: 60 ms, Memory: 69976 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.swift
@@ -0,0 +1,93 @@
+struct Heap<T> where T: Comparable {
+    private var elements: [T] = []
+    private let sortCriteria: (T, T) -> Bool
+    
+    init(sort: @escaping (T, T) -> Bool) {
+        self.sortCriteria = sort
+    }
+    
+    var isEmpty: Bool {
+        return elements.isEmpty
+    }
+    
+    var count: Int {
+        return elements.count
+    }
+    
+    mutating func insert(_ value: T) {
+        elements.append(value)
+        shiftUp(from: elements.count - 1)
+        
+    }
+    
+    mutating func remove() -> T? {
+        guard !elements.isEmpty else { return nil }
+        if elements.count == 1 {
+            return elements.removeFirst()
+        } else {
+            let root = elements[0]
+            elements[0] = elements.removeLast()
+            shiftDown(from: 0)
+            return root
+        }
+    }
+    
+    private mutating func shiftUp(from index: Int) {
+        var child = index
+        let childValue = elements[child]
+        var parent = (child - 1) / 2
+        
+        while child > 0 && sortCriteria(childValue, elements[parent]) {
+            elements.swapAt(child, parent)
+            child = parent
+            parent = (child - 1) / 2
+        }
+    }
+    
+    private mutating func shiftDown(from index: Int) {
+        let count = elements.count
+        var parent = index
+        
+        while true {
+            let left = parent * 2 + 1
+            let right = left + 1
+            var candidate = parent
+            
+            if left < count && sortCriteria(elements[left], elements[candidate]) {
+                candidate = left
+            }
+            
+            if right < count && sortCriteria(elements[right], elements[candidate]) {
+                candidate = right
+            }
+            
+            if candidate == parent { break }
+            
+            elements.swapAt(parent, candidate)
+            parent = candidate
+        }
+    }
+}
+
+var heap = Heap<Int> {
+    if abs($0) < abs($1) {
+        return true
+    } else if abs($0) == abs($1) {
+        return $0 < $1
+    } else {
+        return false
+    }
+}
+
+
+let n = Int(String(readLine()!))!
+var result = ""
+for _ in 0..<n {
+    let input = Int(readLine()!)!
+    if input == 0 {
+        result += "\(heap.remove() ?? 0)\n"
+    } else {
+        heap.insert(input)
+    }
+}
+print(result)
