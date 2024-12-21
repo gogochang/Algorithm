6d0bcfdf802dfb9db7e0f3a8d9709ee43ed971d6
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-12-21 13:05:00 +0900

    [Gold IV] Title: 공유기 설치, Time: 96 ms, Memory: 72236 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.swift
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.swift
@@ -1,46 +1,41 @@
-let input = readLine()!.split(separator: " ").map { Int($0)!}
+let input = readLine()!.split(separator: " ").map { Int($0)! }
 let n = input[0] // 집의 개수
-let c = input[1] // 공유기의 개수
-
-var housePositions = [Int]() //집의 위치 정보 배열
+let c = input[1] // 공유기 개수
+var houses: [Int] = []
 for _ in 0..<n {
-    housePositions.append(Int(readLine()!)!)
+    houses.append(Int(readLine()!)!)
 }
-// 1, 2, 4, 8, 9
-housePositions.sort()
+houses.sort()
 
 func canInstallRoutes(minDistance: Int) -> Bool {
-    var count = 1 // 첫 번째 집에 공유기를 설치
-    var lastInstalled = housePositions[0] // 마지막으로 설치한 위치
+    var count = 1
+    var lastInstalled = houses[0]
     
-    for i in 1..<housePositions.count {
-        if housePositions[i] - lastInstalled >= minDistance {
+    for i in 1..<houses.count {
+        if houses[i] - lastInstalled >= minDistance {
             count += 1
-            lastInstalled = housePositions[i]
+            lastInstalled = houses[i]
         }
         
-        if count >= c {
+        if count == c {
             return true
         }
     }
-    
     return false
 }
 
-//이분 탐색
-var left = 1 // 최소거래
-var right = housePositions.last! - housePositions.first! // 최대 거리
+var left = 1
+var right = houses.last! - houses.first!
 var result = 0
-
 while left <= right {
     let mid = (left + right) / 2
-    
     if canInstallRoutes(minDistance: mid) {
-        result = mid // 가능한 거리라면 갱신
-        left = mid + 1 // 더 큰 거리 탐색
+        result = mid
+        left = mid + 1
     } else {
-        right = mid - 1 // 더 적은 거리 탐색
+        right = mid - 1
     }
+    
 }
 
 print(result)
