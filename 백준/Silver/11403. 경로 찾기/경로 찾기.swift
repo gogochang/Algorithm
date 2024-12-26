3486f2309c3b41b8c77f8360a904abfc60fb2eac
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2024-12-26 13:37:06 +0900

    [Silver I] Title: 경로 찾기, Time: 12 ms, Memory: 69240 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.swift
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.swift
@@ -1,22 +1,31 @@
+// 입력 처리
 let n = Int(readLine()!)!
-var graph = [[Int]]()
-
+var graph: [[Int]] = []
 for _ in 0..<n {
     let row = readLine()!.split(separator: " ").map { Int($0)! }
     graph.append(row)
 }
 
-// 플로이드-워셜 알고리즘
-for k in 0..<n {
-    for i in 0..<n {
-        for j in 0..<n {
-            if graph[i][k] == 1 && graph[k][j] == 1 {
-                graph[i][j] = 1
-            }
+// 결과를 담을 배열
+var result: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: n)
+
+// DFS
+func dfs(start: Int, current: Int, visited: inout[Bool]) {
+    for next in 0..<n {
+        if graph[current][next] == 1 && !visited[next] {
+            visited[next] = true
+            result[start][next] = 1
+            dfs(start: start, current: next, visited: &visited)
         }
     }
 }
 
-for row in graph {
+for i in 0..<n {
+    var visited: [Bool] = Array(repeating: false, count: n)
+    dfs(start: i, current: i, visited: &visited)
+}
+
+// 결과 출력
+for row in result {
     print(row.map { String($0) }.joined(separator: " "))
 }
