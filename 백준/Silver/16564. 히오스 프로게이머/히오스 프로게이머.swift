f635fdd98812c6b8fd268c7db81c15725c92cf1f
ChangGyuKim <54075367+gogochang@users.noreply.github.com>
2025-01-06 22:50:16 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 56 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.swift
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.swift
@@ -1,34 +1,28 @@
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let n = input[0] // 캐릭터의 개수
 let k = input[1] // 올릴 수 있는 레벨 총합
-
 var levels = [Int]()
 for _ in 0..<n {
     levels.append(Int(readLine()!)!)
 }
-levels.sort()
 
-func calculateNum(target: Int) -> Int {
+func calulateNum(target: Int) -> Int {
     var result = 0
     
     for level in levels {
-        if level < target {
-            result += target - level
-        } else {
-            break
-        }
+        let num = target - level
+        result += num > 0 ? num : 0
     }
     
     return result
 }
-
-var left = levels[0]
+var left = levels.min()!
 var right = levels.max()! + k
 var result = left
 
 while left <= right {
     let mid = (left + right) / 2
-    let currentNum = calculateNum(target: mid)
+    let currentNum = calulateNum(target: mid)
     
     if currentNum <= k {
         result = mid
